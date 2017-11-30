2185a9723c92ce30ae2dd2319111403bcd98f570
miketheo423 <miketheo423@gmail.com>
2017-11-30 10:57:03 -0700

    Map is showing up in view

--- components/Map.js
+++ components/Map.js
@@ -7,7 +7,9 @@ import {
   Button
 } from 'react-native';
 
-export default class Map extends Component<{}> {
+import MapView from 'react-native-maps';
+
+export default class Map extends Component <{}> {
 	static navigationOptions = {
 		tabBarLabel: 'Map',
 		tabBarIcon: ({tintColor}) => (
@@ -20,16 +22,51 @@ export default class Map extends Component<{}> {
 	render() {
 		return(
 			<View style={styles.container}>
-				<Text style={{fontSize: 30}}>This is the Map page!</Text>
+				<MapView
+					style={styles.map}
+					initialRegion={{
+						latitude: 39.749632,
+						longitude: -105.000363,
+						latitudeDelta: 0.0222,
+						longitudeDelta: 0.0201,
+					}}
+				/>
 			</View>
 		)
 	}
 }
 
+// export default class Map extends Component<{}> {
+	// static navigationOptions = {
+	// 	tabBarLabel: 'Map',
+	// 	tabBarIcon: ({tintColor}) => (
+	// 		<Image
+	// 			source={require('../images/map-icon.png')}
+	// 			style={{width: 22, height: 22, tintColor: 'white'}}>
+	// 		</Image>
+	// 	)
+	// }
+// 	render() {
+// 		return(
+// 			<View style={styles.container}>
+// 				<Text style={{fontSize: 30}}>This is the Map page!</Text>
+// 			</View>
+// 		)
+// 	}
+// }
+
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		justifyContent: 'center',
-		alignItems: 'center'
+		alignItems: 'center',
+		backgroundColor: '#F5FCFF',
 	},
+	map: {
+		position: "absolute",
+		top: 0,
+		left: 0,
+		right: 0,
+		bottom: 0,
+	}
 })
